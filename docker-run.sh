f81315f654af3d68e1593c8cc9bcd10fa560de68
Ashutosh Nath Agarwal <ashutosh.nath.agarwal@gmail.com>
2018-12-13 13:35:43 -0800

    docker run file added

---
+++ docker-run.sh
@@ -0,0 +1,18 @@
+echo '---------------------------------'
+echo '=====>run Portfolio-NewsAnchor'
+echo '---------------------------------'
+
+echo '=====>Removing existing image if exists...'
+docker stop portfolio-newsanchor
+docker rm portfolio-newsanchor
+sleep 2
+
+echo '=====>docker build'
+docker build --no-cache -t portfolio-newsanchor .
+
+echo '=====>docker run'
+docker run -d --restart always --name portfolio-newsanchor -p 8080:8080 -e "NODE_ENV=local" portfolio-newsanchor
+
+echo '---------------------------------'
+echo '=====>Portfolio-NewsAnchor running on port 8080'
+echo '---------------------------------'
